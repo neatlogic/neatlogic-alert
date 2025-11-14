5766c0c6d10cada1f020c3cc03ff1ef96c6be07b
chenqw <gdnpc@163.com>
2025-11-14 15:48:06 +0800

    adjust
    Signed-off-by: chenqw <gdnpc@163.com>

--- src/main/java/neatlogic/module/alert/api/alert/HandleAlertApi.java
+++ src/main/java/neatlogic/module/alert/api/alert/HandleAlertApi.java
@@ -13,15 +13,21 @@
 package neatlogic.module.alert.api.alert;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.alert.auth.ALERT_BASE;
 import neatlogic.framework.alert.dto.AlertVo;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.common.constvalue.ApiParamType;
-import neatlogic.framework.restful.annotation.*;
+import neatlogic.framework.exception.type.ParamNotExistsException;
+import neatlogic.framework.restful.annotation.Description;
+import neatlogic.framework.restful.annotation.Input;
+import neatlogic.framework.restful.annotation.OperationType;
+import neatlogic.framework.restful.annotation.Param;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import neatlogic.module.alert.service.IAlertService;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -50,19 +56,32 @@ public class HandleAlertApi extends PrivateApiComponentBase {
     }
 
     @Input({
-            @Param(name = "id", desc = "id", isRequired = true, type = ApiParamType.LONG),
+            @Param(name = "id", desc = "id", type = ApiParamType.LONG),
+            @Param(name = "idList", desc = "id列表", type = ApiParamType.JSONARRAY),
             @Param(name = "status", desc = "状态", isRequired = true, type = ApiParamType.STRING),
             @Param(name = "isClose", desc = "是否关闭", isRequired = true, type = ApiParamType.INTEGER),
             @Param(name = "comment", desc = "评论", type = ApiParamType.STRING),
             @Param(name = "isChangeChildAlertStatus", desc = "是否更新子告警状态", type = ApiParamType.INTEGER)
     })
-    @Output({@Param(name = "id", desc = "告警id", type = ApiParamType.LONG)})
     @Description(desc = "处理告警")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
-        AlertVo alertVo = JSON.toJavaObject(jsonObj, AlertVo.class);
-        alertService.handleAlert(alertVo);
-        return alertVo.getId();
+        Long id = jsonObj.getLong("id");
+        JSONArray idList = jsonObj.getJSONArray("idList");
+        if (id == null && CollectionUtils.isEmpty(idList)) {
+            throw new ParamNotExistsException("id", "idList");
+        }
+        if (id != null) {
+            AlertVo alertVo = JSON.toJavaObject(jsonObj, AlertVo.class);
+            alertService.handleAlert(alertVo);
+        } else if (CollectionUtils.isNotEmpty(idList)) {
+            for (int i = 0; i < idList.size(); i++) {
+                AlertVo alertVo = JSON.toJavaObject(jsonObj, AlertVo.class);
+                alertVo.setId(idList.getLong(i));
+                alertService.handleAlert(alertVo);
+            }
+        }
+        return null;
     }
 
 
