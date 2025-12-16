485b98641e577b3b1cd68aa26180cfdd4fd1ed93
lvzk <897706680@qq.com>
2025-12-16 15:36:01 +0800

    update changelog sql

--- src/main/resources/neatlogic/resources/alert/changelog/2025-04-01/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/alert/changelog/2025-04-01/neatlogic_tenant.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `alert_attrtype`
-    ADD COLUMN `sort`   int     NULL COMMENT '排序' AFTER `is_normal`,
+    ADD COLUMN `sort`   int     NULL COMMENT '排序' AFTER `is_normal`;
+ALTER TABLE `alert_attrtype`
     ADD COLUMN `is_top` tinyint NULL COMMENT '是否置顶' AFTER `sort`;
